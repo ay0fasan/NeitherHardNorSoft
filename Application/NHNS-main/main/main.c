46c90747d1cfdb9373ea08dc33e42601ec82dbc1
ay0fasan <ahfasan@uwaterloo.ca>
2025-03-22 23:45:59 -0400

    [NHNS-15] Update main.c

--- Application/NHNS-main/main/main.c
+++ Application/NHNS-main/main/main.c
@@ -3,6 +3,7 @@
 #include "nhns_status_codes.h"
 #include "board.h"
 #include "logger.h"
+#include "os.h"
 
 // --- Defines ---
 
@@ -12,8 +13,18 @@
 
 // --- Functions ---
 
+void StartDefaultTask(void *argument)
+{
+    while (1)
+    {
+        OS_DelayMs(2000);
+    }
+}
+
 int main(void)
 {
+    nhns_status_t nRet = NHNS_STATUS_OK;
+
     // 1) STM32 HAL library initialization
     HAL_Init();
 
@@ -21,9 +32,29 @@ int main(void)
     SystemClock_Config();
 
     // 3) Initialize the logger
-    Logger_Init();
+    nRet = Logger_Init();
+    if (nRet != NHNS_STATUS_OK)
+    {
+        return nRet;
+    }
 
-    // 4) Main loop - we should not get here as OS has taken over
+    // 4) Create the default thread
+    nRet = OS_TaskCreate(TASK_ID_DEFAULT, StartDefaultTask);
+    if (nRet != NHNS_STATUS_OK)
+    {
+        LOGGER_LOG("%d", nRet);
+        return nRet;
+    }
+
+    // 5) Start scheduler
+    nRet = OS_SchedulerInit();
+    if (nRet != NHNS_STATUS_OK)
+    {
+        LOGGER_LOG("%d", nRet);
+        return nRet;
+    }
+
+    // 6) Main loop - we should not get here as OS has taken over
     while (1)
     {
         // Do nothing
